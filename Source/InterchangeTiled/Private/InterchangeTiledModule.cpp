2e8d074f94fc1bd963d4b7b1b4576c852dc83a72
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2026-09-25 23:27:56 +0200

    Register translator pipelines on the default stack at startup
    Adds the InterchangeTmxTranslator and InterchangeTsxTranslator to the
    PerTranslatorPipelines of the current default pipeline stack, using the
    pipeline class default objects. This removes the need to manually create
    pipeline assets and wire them up in Project Settings, so importing works
    immediately after enabling the plugin.

--- Source/InterchangeTiled/Private/InterchangeTiledModule.cpp
+++ Source/InterchangeTiled/Private/InterchangeTiledModule.cpp
@@ -1,6 +1,7 @@
 #include "InterchangeTiledModule.h"
 
 #include "InterchangeManager.h"
+#include "InterchangeProjectSettings.h"
 #include "InterchangeTileMapFactory.h"
 #include "InterchangeTileSetFactory.h"
 #include "InterchangeTmxTranslator.h"
@@ -11,6 +12,27 @@ IMPLEMENT_MODULE(FInterchangeTiled, InterchangeTiled);
 
 DEFINE_LOG_CATEGORY(LogInterchangeTiledImport);
 
+namespace
+{
+	void RegisterTranslatorPipeline(
+		FInterchangePipelineStack& PipelineStack,
+		const TSoftClassPtr<UInterchangeTranslatorBase>& Translator,
+		const TCHAR* PipelinePath)
+	{
+		FInterchangeTranslatorPipelines* Entry = PipelineStack.PerTranslatorPipelines.FindByPredicate(
+			[&Translator](const FInterchangeTranslatorPipelines& Item)
+			{
+				return Item.Translator == Translator;
+			});
+		if (!Entry)
+		{
+			Entry = &PipelineStack.PerTranslatorPipelines.AddDefaulted_GetRef();
+			Entry->Translator = Translator;
+		}
+		Entry->Pipelines.AddUnique(FSoftObjectPath(PipelinePath));
+	}
+}
+
 void FInterchangeTiled::StartupModule()
 {
 	UInterchangeManager& InterchangeManager = UInterchangeManager::GetInterchangeManager();
@@ -20,6 +42,23 @@ void FInterchangeTiled::StartupModule()
 
 	InterchangeManager.RegisterFactory(UInterchangeTileMapFactory::StaticClass());
 	InterchangeManager.RegisterFactory(UInterchangeTileSetFactory::StaticClass());
+
+	// Register the default pipelines for our translators on the default
+	// pipeline stack, so imports work without manual project setup.
+	FInterchangeImportSettings& ImportSettings =
+		FInterchangeProjectSettingsUtils::GetMutableDefaultImportSettings(false);
+	if (FInterchangePipelineStack* PipelineStack =
+			ImportSettings.PipelineStacks.Find(ImportSettings.DefaultPipelineStack))
+	{
+		RegisterTranslatorPipeline(
+			*PipelineStack,
+			UInterchangeTmxTranslator::StaticClass(),
+			TEXT("/Script/InterchangeTiled.Default__InterchangeTmxPipeline"));
+		RegisterTranslatorPipeline(
+			*PipelineStack,
+			UInterchangeTsxTranslator::StaticClass(),
+			TEXT("/Script/InterchangeTiled.Default__InterchangeTsxPipeline"));
+	}
 }
 
 void FInterchangeTiled::ShutdownModule()
